2dc1184eb53348cd5fcdc24b6220bd1b9bfe8283
alluring-blog-5170 <knikadass@gmail.com>
2020-07-07 15:47:29 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,6 @@
-def introduction_with_language(name, language)
-  puts "Hi, ny name is #{name} and I am learning to program in #{language}."
-end
+  def introduction_with_language(name, language)
+    puts "Hi, ny name is #{name} and I am learning to program in #{language}."
+  end
+ end
+introduction_with_language
+ introduction_with_language
